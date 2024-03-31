be27580ac353f84ea2a2f75badaaa0a883e26930
Zhang ShuoFeng <852333743@qq.com>
2024-03-31 23:22:43 +0800

    change _site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1711891637',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
---
+++ _site/old/README.md
@@ -0,0 +1,3 @@
+# cancersyx.github.io
+my blog website
+本次网站的搭建主要参考http://www.54tianzhisheng.cn/Blog/html/blog_github.html。
