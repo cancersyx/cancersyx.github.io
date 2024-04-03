28824640509462763d3e6e1b9e0ed9e473966257
Zhang ShuoFeng <852333743@qq.com>
2024-04-03 14:19:03 +0800

    update ignore file

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712121915',resources: [
+    cacheName: 'chirpy-1712125131',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- old/README.md
+++
@@ -1,3 +0,0 @@
-# cancersyx.github.io
-my blog website
-本次网站的搭建主要参考http://www.54tianzhisheng.cn/Blog/html/blog_github.html。
