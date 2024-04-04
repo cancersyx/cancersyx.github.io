41d5d0d17859074c00e4df255a855d9dbac5f7bf
Zhang ShuoFeng <852333743@qq.com>
2024-04-04 11:25:48 +0800

    update files

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712137184',resources: [
+    cacheName: 'chirpy-1712201134',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
