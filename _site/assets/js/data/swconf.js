2199daf06e023988770d73d78fedab3bd87fddb4
Zhang ShuoFeng <852333743@qq.com>
2024-04-04 16:03:02 +0800

    delete CNAME

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712212497',resources: [
+    cacheName: 'chirpy-1712217725',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
